5e01dc842be0f417f76592ce16bf522145bf85ef
MathieuNassar <mathieu.nassar@gmail.com>
2020-10-04 13:27:36 +0200

    FIX: links

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -38,10 +38,10 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
-    {doc: 'ghostmodule', label: 'Guides'},
+    {doc: 'module', label: 'Guides'},
     {doc: 'tutorial_setup', label: 'Tutorials'},
     {page: 'help', label: 'Help'},
-    {blog: true, label: 'Blog'},
+    {blog: false, label: 'Blog'},
   ],
 
   // If you have users set above, you add it here:
