422a69d0d86f6f9d9965b1725a4c231afc953326
MathieuNassar <mathieu.nassar@gmail.com>
2020-10-04 12:50:54 +0200

    ADD: link to github

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -64,7 +64,7 @@ class HomeSplash extends React.Component {
         <div className="inner">
           <ProjectTitle siteConfig={siteConfig} />
           <PromoSection>
-          	<a href="https://github.com/mathieunassar/ghostmodule"><img src="/ghost-docs/img/GitHub-Mark-32px.png" /></a>
+          	<a href="https://github.com/mathieunassar/ghostmodule"><img src="/ghost-docs/img/GitHub-Mark-Light-32px.png" /></a>
             <Button href={docUrl('module')}>Guides</Button>
             <Button href={docUrl('tutorial_setup')}>Tutorials</Button>
           </PromoSection>
